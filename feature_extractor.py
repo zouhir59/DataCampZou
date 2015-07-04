778af07c8e439b0c127f131eb8ab31d61e9d284b
zouhir59 <z.oumedjkane@gmail.com>
2015-07-04 11:57:04 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,5 +1,6 @@
-import pandas as pd
+import pandas as pd 
 import os
+ 
  
 class FeatureExtractor(object):
     def __init__(self):
@@ -10,17 +11,33 @@ class FeatureExtractor(object):
  
     def transform(self, X_df):
         X_encoded = X_df
-        path = os.path.dirname(__file__)
-        data_weather = pd.read_csv(os.path.join(path, "data_weather.csv"))
-        X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
-        X_encoded = X_encoded.merge(X_weather, how='left',
-            left_on=['DateOfDeparture', 'Arrival'], 
-            right_on=['Date', 'AirPort'], sort=False)
-         
+        
+        #uncomment the line below in the submission
+        #path = os.path.dirname(__file__)
+        X_encoded = X_df
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop(['Arrival', 'Departure', 
-            'DateOfDeparture', 'Date', 'AirPort'], axis=1)
- 
+        X_encoded = X_encoded.drop('Departure', axis=1)
+        X_encoded = X_encoded.drop('Arrival', axis=1)
+    
+     #   data_holidays = pd.read_csv(os.path.join(path, "data_holidays.csv"))
+     #   X_holidays = data_holidays[['DateOfDeparture','Xmas','Xmas-1','NYD','NYD-1','Ind','Thg','Thg+1']]
+     #   X_encoded = X_encoded.set_index(['DateOfDeparture'])
+     #   X_holidays = X_holidays.set_index(['DateOfDeparture'])
+     #   X_encoded = X_encoded.join(X_holidays).reset_index()        
+        
+        X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
+        X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
+        X_encoded['weekday'] = X_encoded['DateOfDeparture'].dt.weekday
+        X_encoded['week'] = X_encoded['DateOfDeparture'].dt.week
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['year'], prefix='y'))
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
+        X_encoded = X_encoded.drop('weekday', axis=1)
+        X_encoded = X_encoded.drop('week', axis=1)
+        X_encoded = X_encoded.drop('year', axis=1)
+        X_encoded = X_encoded.drop('std_wtd', axis=1)
+        X_encoded = X_encoded.drop('WeeksToDeparture', axis=1)        
+        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)     
         X_array = X_encoded.values
         return X_array
