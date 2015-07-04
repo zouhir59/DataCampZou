640390e5683306c1253ea7f5a7efc4eff539f2ec
zouhir59 <z.oumedjkane@gmail.com>
2015-07-04 11:57:59 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -1,13 +1,21 @@
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
-
-
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator
+from sklearn.decomposition import KernelPCA
+from sklearn import neighbors
+from sklearn.decomposition import PCA
+import xgboost as xgb
+ 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1500 , max_depth = 7 , max_features = 15)
-
+        self.clf = xgb.XGBRegressor(max_depth=25, n_estimators=1600, learning_rate=0.05)
+ 
     def fit(self, X, y):
         self.clf.fit(X, y)
-
+ 
     def predict(self, X):
         return self.clf.predict(X)
